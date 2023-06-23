868d1532a711e584535141e3d7aa2e761999400e
nex0 <nicog710@gmail.com>
2023-06-23 02:49:00 -0300

    commit
    commit

---
+++ Obligatorio_BaseDeDatos2023/FormulaUno.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+
+namespace Obligatorio_BaseDeDatos2023
+{
+	public class FormulaUno
+	{
+		public FormulaUno()
+		{
+        }
+
+        public DataTable ReadFile(string path, string tableName)
+        {
+
+            DataTable table = new DataTable(tableName);
+
+            using (StreamReader csvReader = new StreamReader(path))
+            {
+                string[] headers = csvReader.ReadLine().Split(',');
+
+                foreach (string header in headers)
+                {
+                    table.Columns.Add(header);
+                }
+
+                while (!csvReader.EndOfStream)
+                {
+                    string[] lineValues = csvReader.ReadLine().Split(',');
+
+                    DataRow tableRow = table.NewRow();
+
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        string value = lineValues[i].Trim('\"');
+
+                        tableRow[i] = value == "\\N" ? DBNull.Value : (object)value;
+                    }
+                    table.Rows.Add(tableRow);
+
+                }
+
+            }
+            return table;
+        }
+
+        public void InsertData(MySqlConnection connection, DataTable table)
+        {
+
+            try
+            {
+                string tableName = table.TableName;
+                var columnNames = table.Columns;
+                var columnsRows = table.Rows;
+
+                StringBuilder columnNamesConcad = new StringBuilder();
+
+                foreach (DataColumn column in columnNames)
+                {
+                    if (columnNamesConcad.Length > 0)
+                    {
+                        columnNamesConcad.Append(',');
+                    }
+                    columnNamesConcad.Append(column.ColumnName);
+                }
+
+                //string columns = columnNamesConcad.ToString();
+
+                string columns = "`" + columnNamesConcad.ToString().Replace(",", "`,`") + "`";
+
+
+                foreach (DataRow row in columnsRows)
+                {
+                    StringBuilder parameters = new StringBuilder();
+                    using (MySqlCommand command = new MySqlCommand())
+                    {
+                        command.Connection = connection;
+
+                        foreach (DataColumn column in columnNames)
+                        {
+                            if (parameters.Length > 0)
+                            {
+                                parameters.Append(',');
+                            }
+
+                            string parameterName = $"@{column.ColumnName}";
+                            parameters.Append(parameterName);
+
+                            command.Parameters.AddWithValue(parameterName, row[column.ColumnName] ?? DBNull.Value);
+                        }
+
+                        command.CommandText = $"INSERT INTO {tableName} ({columns}) VALUES ({parameters})";
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
+
---
+++ Obligatorio_BaseDeDatos2023/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Data;
+
+using System;
+using Obligatorio_BaseDeDatos2023;
+using MySql.Data.MySqlClient;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                FormulaUno formulaUno = new FormulaUno();
+                string connectionString = "server=localhost;user=root;password=bernardo;database=formulaUno";
+
+                using (MySqlConnection conecction = new MySqlConnection(connectionString))
+                {
+                    conecction.Open();
+
+                    DataTable drivers = formulaUno.ReadFile("/Users/nexo/Downloads/archive/drivers.csv", "drivers");
+                    formulaUno.InsertData(conecction, drivers);
+
+                    DataTable constructors = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructors.csv", "constructors");
+                    formulaUno.InsertData(conecction, constructors);
+
+                    DataTable status = formulaUno.ReadFile("/Users/nexo/Downloads/archive/status.csv", "status");
+                    formulaUno.InsertData(conecction, status);
+
+                    DataTable seasons = formulaUno.ReadFile("/Users/nexo/Downloads/archive/seasons.csv", "seasons");
+                    formulaUno.InsertData(conecction, seasons);
+
+                    DataTable circuits = formulaUno.ReadFile("/Users/nexo/Downloads/archive/circuits.csv", "circuits");
+                    formulaUno.InsertData(conecction, circuits);
+
+                    DataTable races = formulaUno.ReadFile("/Users/nexo/Downloads/archive/races.csv", "races");
+                    formulaUno.InsertData(conecction, races);
+
+                    DataTable constructor_results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructor_results.csv", "constructor_results");
+                    formulaUno.InsertData(conecction, constructor_results);
+
+                    DataTable constructor_standings = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructor_standings.csv", "constructor_standings");
+                    formulaUno.InsertData(conecction, constructor_standings);
+
+                    DataTable driver_standings = formulaUno.ReadFile("/Users/nexo/Downloads/archive/driver_standings.csv", "driver_standings");
+                    formulaUno.InsertData(conecction, driver_standings);
+
+                    DataTable lap_times = formulaUno.ReadFile("/Users/nexo/Downloads/archive/lap_times.csv", "lap_times");
+                    formulaUno.InsertData(conecction, lap_times);
+
+                    DataTable pit_stops = formulaUno.ReadFile("/Users/nexo/Downloads/archive/pit_stops.csv", "pit_stops");
+                    formulaUno.InsertData(conecction, pit_stops);
+
+                    DataTable qualifying = formulaUno.ReadFile("/Users/nexo/Downloads/archive/qualifying.csv", "qualifying");
+                    formulaUno.InsertData(conecction, qualifying);
+
+                    DataTable results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/results.csv", "results");
+                    formulaUno.InsertData(conecction, results);
+
+                    DataTable sprint_results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/sprint_results.csv", "sprint_results");
+                    formulaUno.InsertData(conecction, sprint_results);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+    }
+}
+
