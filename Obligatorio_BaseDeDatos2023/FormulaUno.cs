a5cb15ed86994b34298af27bdb00247ae9b5a1de
nex0 <nicog710@gmail.com>
2023-07-03 19:38:40 -0300

    commit

--- Obligatorio_BaseDeDatos2023/FormulaUno.cs
+++ Obligatorio_BaseDeDatos2023/FormulaUno.cs
@@ -2,7 +2,6 @@
 using System.Data;
 using System.Text;
 using MySql.Data.MySqlClient;
-using MySqlX.XDevAPI.Relational;
 
 namespace Obligatorio_BaseDeDatos2023
 {
@@ -12,7 +11,6 @@ namespace Obligatorio_BaseDeDatos2023
 		{
         }
 
-        //Método para leer los datos de un archivo CSV y convertirlos en una DataTable.
         public DataTable ReadFile(string path, string tableName)
         {
             DataTable table = new DataTable(tableName);
@@ -53,7 +51,7 @@ namespace Obligatorio_BaseDeDatos2023
             return table;
         }
 
-        // Método para insertar los datos de una DataTable en una base de datos MySQL.
+
         public void InsertData(MySqlConnection connection, DataTable table)
         {
             try
@@ -116,6 +114,181 @@ namespace Obligatorio_BaseDeDatos2023
         }
 
 
+        private DataTable ExecuteQuery(MySqlConnection connection, string query)
+        {
+            DataTable dt = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        //1.Qué piloto es el que ha ganado más campeonatos mundiales? (Si hay fanáticos, podrán validar los 7 campeonatos de Lewis y de Michael)
+        public DataTable PilotoMasCampeonatosMundiales(MySqlConnection connection)
+        {
+            string query = @"
+                    SELECT piloto, COUNT(*) AS total_campeonatos
+                    FROM (
+                    SELECT r.year, CONCAT(d.forename, ' ', d.surname) AS piloto, ds.points AS max_points
+                    FROM races r
+                    JOIN driver_standings ds ON r.raceId = ds.raceId
+                    JOIN drivers d ON ds.driverId = d.driverId
+                    WHERE (r.year, r.round) IN (
+                    SELECT year, MAX(round)
+                    FROM races
+                    GROUP BY year
+                    )
+                    AND ds.points = (
+                    SELECT MAX(ds2.points)
+                    FROM driver_standings ds2
+                    WHERE ds2.raceId = ds.raceId
+                    )
+                    ) AS tmp
+                    GROUP BY piloto
+                    ORDER BY total_campeonatos DESC
+                    LIMIT 2";
+            return ExecuteQuery(connection, query);
+        }
+
+        //2. Qué escudería ha ganado el campeonato de constructores más veces en la historia? 
+        public DataTable EscuderiaMasCampeonatos(MySqlConnection connection)
+        {
+            string query = @"
+                SELECT ganador, COUNT(*) as total_victorias
+                FROM (
+                SELECT carreras.year, constructores.name as ganador, MAX(cs.points) as puntos
+                FROM constructor_standings cs
+                JOIN races carreras ON cs.raceId = carreras.raceId
+                JOIN constructors constructores ON cs.constructorId = constructores.constructorId
+                WHERE (carreras.year, cs.points) IN (
+                SELECT carreras.year, MAX(cs.points)
+                FROM constructor_standings cs
+                JOIN races carreras ON cs.raceId = carreras.raceId
+                GROUP BY carreras.year
+                )
+                GROUP BY carreras.year, constructores.name
+                ) as campeonatos
+                GROUP BY ganador
+                ORDER BY total_victorias DESC
+                LIMIT 1";
+            return ExecuteQuery(connection, query);
+        }
+
+        //3. Qué piloto ganó más veces un Gran Premio?
+        public DataTable PilotoQueGanoMasUnGranPremio(MySqlConnection connection)
+        {
+            string query = @"
+                        SELECT
+                        pilotos.driverId,
+                        pilotos.forename,
+                        pilotos.surname,
+                        COUNT(*) as ganadas
+                        FROM results resultados
+                        JOIN drivers pilotos on resultados.driverId = pilotos.driverId
+                        WHERE positionOrder = 1
+                        GROUP BY pilotos.driverId
+                        ORDER BY ganadas DESC
+                        LIMIT 1";
+            return ExecuteQuery(connection, query);
+        }
+
+        //4. Qué grandes premios tuvieron los campeonatos del 1996 – 1999. 
+        public DataTable GrandesPremiosCampeonatos(MySqlConnection connection)
+        {
+            string query = @"
+                        SELECT
+                        carreras.year,
+                        circuitos.name
+                        FROM
+                        races carreras
+                        JOIN
+                        circuits circuitos ON carreras.circuitId = circuitos.circuitId
+                        WHERE
+                        carreras.year BETWEEN 1996 AND 1999
+                        ORDER BY
+                        carreras.year, circuitos.name";
+            return ExecuteQuery(connection, query);
+        }
+
+        //5. Quién ganó el Gran Premio de Suzuka, de 1997? 
+        public DataTable GanadorSuzuka1997(MySqlConnection connection)
+        {
+            string query = @"
+                        SELECT
+                        pilotos.driverId,
+                        pilotos.forename,
+                        pilotos.surname
+                        FROM
+                        results resultados
+                        JOIN
+                        drivers pilotos ON resultados.driverId = pilotos.driverId
+                        WHERE
+                        resultados.raceId = (
+                        SELECT
+                        carreras.raceId
+                        FROM
+                        races carreras
+                        JOIN
+                        circuits circuitos ON carreras.circuitId = circuitos.circuitId
+                        WHERE
+                        circuitos.name = 'Suzuka Circuit' AND carreras.year = 1997
+                        )
+                        AND resultados.positionOrder = 1";
+            return ExecuteQuery(connection, query);
+        }
+
+        //6. Cuántas carreras ganó Jacques Villenueve? 
+        public DataTable CarrerasGanadasVilleneuve(MySqlConnection connection)
+        {
+            string query = @"
+                    SELECT
+                    pilotos.driverId,
+                    pilotos.forename,
+                    pilotos.surname,
+                    COUNT(*) as ganadas
+                    FROM results resultados
+                    JOIN drivers pilotos on resultados.driverId = pilotos.driverId
+                    WHERE pilotos.surname = 'Villeneuve' AND pilotos.forename = 'Jacques' AND positionOrder = 1
+                    GROUP BY pilotos.driverId
+                    ORDER BY ganadas DESC
+                    LIMIT 1";
+            return ExecuteQuery(connection, query);
+        }
+
+        //7. Quién ganó más carreras saliendo desde una posición que no era la Pole Position?
+        public DataTable MasGanadorNoPole(MySqlConnection connection)
+        {
+            string query = @"
+                        SELECT
+                        pilotos.driverId,
+                        pilotos.forename,
+                        pilotos.surname,
+                        COUNT(*) as ganadas
+                        FROM results resultados
+                        JOIN drivers pilotos on resultados.driverId = pilotos.driverId
+                        WHERE grid != 1 AND positionOrder = 1
+                        GROUP BY pilotos.driverId
+                        ORDER BY ganadas DESC
+                        LIMIT 1";
+            return ExecuteQuery(connection, query);
+        }
+
+        //8. Y quién saliendo desde ahí? 
+        public DataTable MasGanadorDesdePole(MySqlConnection connection)
+        {
+            string query = @"
+                        SELECT
+                        pilotos.driverId,
+                        pilotos.forename,
+                        pilotos.surname,
+                        COUNT(*) as ganadas
+                        FROM results resultados
+                        JOIN drivers pilotos on resultados.driverId = pilotos.driverId
+                        WHERE grid = 1 AND positionOrder = 1
+                        GROUP BY pilotos.driverId
+                        ORDER BY ganadas DESC
+                        LIMIT 1";
+            return ExecuteQuery(connection, query);
+        }
     }
 }
 
--- Obligatorio_BaseDeDatos2023/Program.cs
+++ Obligatorio_BaseDeDatos2023/Program.cs
@@ -1,6 +1,5 @@
-﻿using System.Data;
-
-using System;
+﻿using System;
+using System.Data;
 using Obligatorio_BaseDeDatos2023;
 using MySql.Data.MySqlClient;
 
@@ -10,57 +9,78 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            FormulaUno formulaUno = new FormulaUno();
+            string connectionString = "server=localhost;user=root;password=bernardo;database=formulaUno";
 
             try
             {
-                FormulaUno formulaUno = new FormulaUno();
-                string connectionString = "server=localhost;user=root;password=bernardo;database=formulaUno";
-
-                using (MySqlConnection conecction = new MySqlConnection(connectionString))
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    conecction.Open();
+                    connection.Open();
 
-                    DataTable drivers = formulaUno.ReadFile("/Users/nexo/Downloads/archive/drivers.csv", "drivers");
-                    formulaUno.InsertData(conecction, drivers);
+                    bool continuar = true;
+                    while (true)
+                    {
+                        Console.WriteLine("---- Menú ----");
+                        Console.WriteLine("1. Insertar todos los datos.");
+                        Console.WriteLine("2. Ver piloto con más campeonatos mundiales.");
+                        Console.WriteLine("3. Ver escudería con más campeonatos.");
+                        Console.WriteLine("4. Ver piloto que ganó más veces un Gran Premio.");
+                        Console.WriteLine("5. Ver Grandes Premios 1996-1999.");
+                        Console.WriteLine("6. Ver ganador Gran Premio de Suzuka 1997.");
+                        Console.WriteLine("7. Ver carreras ganadas por Jacques Villenueve.");
+                        Console.WriteLine("8. Ver quien ganó más carreras sin estar en Pole Position.");
+                        Console.WriteLine("9. Ver quien ganó más carreras desde la Pole Position.");
+                        Console.WriteLine("0. Salir.");
+                        Console.WriteLine("Elige una opción:");
 
-                    DataTable constructors = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructors.csv", "constructors");
-                    formulaUno.InsertData(conecction, constructors);
+                        var opcion = Console.ReadLine();
 
-                    DataTable status = formulaUno.ReadFile("/Users/nexo/Downloads/archive/status.csv", "status");
-                    formulaUno.InsertData(conecction, status);
-
-                    DataTable seasons = formulaUno.ReadFile("/Users/nexo/Downloads/archive/seasons.csv", "seasons");
-                    formulaUno.InsertData(conecction, seasons);
-
-                    DataTable circuits = formulaUno.ReadFile("/Users/nexo/Downloads/archive/circuits.csv", "circuits");
-                    formulaUno.InsertData(conecction, circuits);
-
-                    DataTable races = formulaUno.ReadFile("/Users/nexo/Downloads/archive/races.csv", "races");
-                    formulaUno.InsertData(conecction, races);
-
-                    DataTable constructor_results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructor_results.csv", "constructor_results");
-                    formulaUno.InsertData(conecction, constructor_results);
-
-                    DataTable constructor_standings = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructor_standings.csv", "constructor_standings");
-                    formulaUno.InsertData(conecction, constructor_standings);
-
-                    DataTable driver_standings = formulaUno.ReadFile("/Users/nexo/Downloads/archive/driver_standings.csv", "driver_standings");
-                    formulaUno.InsertData(conecction, driver_standings);
-
-                    DataTable lap_times = formulaUno.ReadFile("/Users/nexo/Downloads/archive/lap_times.csv", "lap_times");
-                    formulaUno.InsertData(conecction, lap_times);
-
-                    DataTable pit_stops = formulaUno.ReadFile("/Users/nexo/Downloads/archive/pit_stops.csv", "pit_stops");
-                    formulaUno.InsertData(conecction, pit_stops);
-
-                    DataTable qualifying = formulaUno.ReadFile("/Users/nexo/Downloads/archive/qualifying.csv", "qualifying");
-                    formulaUno.InsertData(conecction, qualifying);
-
-                    DataTable results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/results.csv", "results");
-                    formulaUno.InsertData(conecction, results);
-
-                    DataTable sprint_results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/sprint_results.csv", "sprint_results");
-                    formulaUno.InsertData(conecction, sprint_results);
+                        switch (opcion)
+                        {
+                            case "1":
+                                InsertData(formulaUno, connection);
+                                break;
+                            case "2":
+                                DataTable dt = formulaUno.PilotoMasCampeonatosMundiales(connection);
+                                PrintDataTable(dt);
+                                break;
+                            case "3":
+                                DataTable dt2 = formulaUno.EscuderiaMasCampeonatos(connection);
+                                PrintDataTable(dt2);
+                                break;
+                            case "4":
+                                DataTable dt3 = formulaUno.PilotoQueGanoMasUnGranPremio(connection);
+                                PrintDataTable(dt3);
+                                break;
+                            case "5":
+                                DataTable dt4 = formulaUno.GrandesPremiosCampeonatos(connection);
+                                PrintDataTable(dt4);
+                                break;
+                            case "6":
+                                DataTable dt5 = formulaUno.GanadorSuzuka1997(connection);
+                                PrintDataTable(dt5);
+                                break;
+                            case "7":
+                                DataTable dt6 = formulaUno.CarrerasGanadasVilleneuve(connection);
+                                PrintDataTable(dt6);
+                                break;
+                            case "8":
+                                DataTable dt7 = formulaUno.MasGanadorNoPole(connection);
+                                PrintDataTable(dt7);
+                                break;
+                            case "9":
+                                DataTable dt8 = formulaUno.MasGanadorDesdePole(connection);
+                                PrintDataTable(dt8);
+                                break;
+                            case "0":
+                                Console.WriteLine("Adiós!");
+                                return;
+                            default:
+                                Console.WriteLine("Opción no válida. Inténtalo de nuevo.");
+                                break;
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -69,7 +89,64 @@ namespace MyApp // Note: actual namespace depends on the project name.
             }
         }
 
+        static void PrintDataTable(DataTable table)
+        {
+            Console.WriteLine("**************************");
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (DataColumn column in table.Columns)
+                {
+                    Console.Write($"{row[column]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("**************************");
+        }
 
+        static void InsertData(FormulaUno formulaUno, MySqlConnection connection)
+        {
+            DataTable drivers = formulaUno.ReadFile("/Users/nexo/Downloads/archive/drivers.csv", "drivers");
+            formulaUno.InsertData(connection, drivers);
+
+            DataTable constructors = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructors.csv", "constructors");
+            formulaUno.InsertData(connection, constructors);
+
+            DataTable status = formulaUno.ReadFile("/Users/nexo/Downloads/archive/status.csv", "status");
+            formulaUno.InsertData(connection, status);
+
+            DataTable seasons = formulaUno.ReadFile("/Users/nexo/Downloads/archive/seasons.csv", "seasons");
+            formulaUno.InsertData(connection, seasons);
+
+            DataTable circuits = formulaUno.ReadFile("/Users/nexo/Downloads/archive/circuits.csv", "circuits");
+            formulaUno.InsertData(connection, circuits);
+
+            DataTable races = formulaUno.ReadFile("/Users/nexo/Downloads/archive/races.csv", "races");
+            formulaUno.InsertData(connection, races);
+
+            DataTable constructor_results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructor_results.csv", "constructor_results");
+            formulaUno.InsertData(connection, constructor_results);
+
+            DataTable constructor_standings = formulaUno.ReadFile("/Users/nexo/Downloads/archive/constructor_standings.csv", "constructor_standings");
+            formulaUno.InsertData(connection, constructor_standings);
+
+            DataTable driver_standings = formulaUno.ReadFile("/Users/nexo/Downloads/archive/driver_standings.csv", "driver_standings");
+            formulaUno.InsertData(connection, driver_standings);
+
+            DataTable lap_times = formulaUno.ReadFile("/Users/nexo/Downloads/archive/lap_times.csv", "lap_times");
+            formulaUno.InsertData(connection, lap_times);
+
+            DataTable pit_stops = formulaUno.ReadFile("/Users/nexo/Downloads/archive/pit_stops.csv", "pit_stops");
+            formulaUno.InsertData(connection, pit_stops);
+
+            DataTable qualifying = formulaUno.ReadFile("/Users/nexo/Downloads/archive/qualifying.csv", "qualifying");
+            formulaUno.InsertData(connection, qualifying);
+
+            DataTable results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/results.csv", "results");
+            formulaUno.InsertData(connection, results);
+
+            DataTable sprint_results = formulaUno.ReadFile("/Users/nexo/Downloads/archive/sprint_results.csv", "sprint_results");
+            formulaUno.InsertData(connection, sprint_results);
+        }
     }
 }
 
